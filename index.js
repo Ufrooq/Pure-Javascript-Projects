88528f5644a1360ae7dd2635353f8edbda40bb7f
Umar <muhammadshafqatali3@gmail.com>
2023-08-28 15:32:55 +0500

    sdsd

--- index.js
+++
@@ -1,5 +0,0 @@
-const parentDiv = document.querySelector(".week-days");
-console.log(parentDiv.firstElementChild.innerHTML);
-const childs = parentDiv.children;
-const myChildArray = Array.from(childs);
-myChildArray.forEach((elem) => (elem.style.color = "red"));
