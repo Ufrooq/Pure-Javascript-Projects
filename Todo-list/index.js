6fc5ae1cce647c0c3c3c31a472c54ec34478c632
Umar <muhammadshafqatali3@gmail.com>
2023-08-29 01:06:50 +0500

    sdsd

--- Todo-list/index.js
+++ Todo-list/index.js
@@ -1,6 +1,20 @@
 let todoArray = [];
-let addTodoBtn = document.getElementById("add-todo-btn");
 let todoItemDiv = document.getElementById("todo-items");
+let addTodoBtn = document.getElementById("add-todo-btn");
+let formBtn = document.getElementById("submit-todo");
+const colorsArray = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+  "rose",
+  "slate",
+];
+let colorIndex = 0;
 
 function getValue() {
   let value = document.getElementById("input-box").value;
@@ -9,22 +23,34 @@ function getValue() {
 }
 
 function insertTodo(value) {
-  todoArray.push(value);
+  todoArray.push({
+    _id: Math.floor(Math.random() * 100000),
+    todoColor:
+      colorsArray[
+        colorIndex <= colorsArray.length ? colorIndex++ : colorIndex--
+      ],
+    text: value,
+  });
   printTodos();
 }
 
 function printTodos() {
+  let todos = "";
   todoArray.forEach((todoData) => {
-    const todoItem = document.createElement("div");
-    todoItem.innerHTML = `
-            <div id="todo" class="flex bg-rose-500 text-white py-2 px-5 rounded-md shadow-md">
-                <p>${todoData}</p>
-                <button class="ms-6"><i class="fa-solid fa-check"></i></button>
-                <button class="ms-3"><i class="fa-solid fa-delete-left"></i></button>
+    todos += `
+            <div id="todo" class="flex justify-between bg-${todoData.todoColor}-500 text-white py-2 px-5 mt-4 rounded-md shadow-md">
+                <p>${todoData.text}</p>
+                <div>
+                    <button class='ms-6'><i class="fa-solid fa-check"></i></button>
+                    <button class="ms-2"><i class="fa-solid fa-delete-left"></i></button>
+                </div>
             </div>`;
   });
+
+  todoItemDiv.innerHTML = todos;
 }
 
-addTodoBtn.addEventListener("click", () => {
+formBtn.addEventListener("submit", (event) => {
+  event.preventDefault();
   getValue();
 });
