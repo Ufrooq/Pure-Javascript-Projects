2ac5a2abad7bbb5a5b46ac8b5f2dbec0747890ba
Umar <muhammadshafqatali3@gmail.com>
2023-08-28 23:55:54 +0500

    sdsd

--- Todo-list/index.js
+++ Todo-list/index.js
@@ -1,20 +1,30 @@
 let todoArray = [];
+let addTodoBtn = document.getElementById("add-todo-btn");
+let todoItemDiv = document.getElementById("todo-items");
+
 function getValue() {
-  const value = document.getElementById("input-box").value;
-  addTodo(value);
+  let value = document.getElementById("input-box").value;
+  insertTodo(value);
+  value = document.getElementById("input-box").value = "";
 }
 
-function addTodo(value) {
+function insertTodo(value) {
   todoArray.push(value);
   printTodos();
 }
 
 function printTodos() {
-  todoArray.forEach((todo) => {
-    console.log(todo);
+  todoArray.forEach((todoData) => {
+    const todoItem = document.createElement("div");
+    todoItem.innerHTML = `
+            <div id="todo" class="flex bg-rose-500 text-white py-2 px-5 rounded-md shadow-md">
+                <p>${todoData}</p>
+                <button class="ms-6"><i class="fa-solid fa-check"></i></button>
+                <button class="ms-3"><i class="fa-solid fa-delete-left"></i></button>
+            </div>`;
   });
 }
 
-document.addEventListener("click", () => {
+addTodoBtn.addEventListener("click", () => {
   getValue();
 });
